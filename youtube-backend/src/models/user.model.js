5f7d29c05f88c4f7e9c061166c793a23a8418b6b
muhammad-ali79 <alihelium369@gmail.com>
2024-02-11 15:45:02 +0500

    user and video model created

---
+++ youtube-backend/src/models/user.model.js
@@ -0,0 +1,109 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      // if we want to apply search on a field then it is recommned to do index === true
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      time: true,
+    },
+
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+
+    coverImage: {
+      type: String, // cloudinary url
+    },
+
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        // referencing the video model
+        ref: "Video",
+      },
+    ],
+
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// for saving data if password is modified then encrypt password
+userSchema.pre("save", async function (next) {
+  // next() essentially acts as a way to pass control to the next function in the middleware chain or to continue with the operation being performed. If you don't call next(), the middleware chain will be halted, and the save operation (or any subsequent middleware) won't proceed.
+
+  // using next() will look like something when the next() is called then we are telling the mongoose to go to the next middleware or perfroms others operations
+  if (!this.isModified("password")) return next();
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// if user given password is same as password store in db then return true
+userSchema.methods.isPasswordCorrected = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      // this id come from database
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      // this id come from database
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+export const User = mongoose.model("User", userSchema);
---
+++ youtube-backend/src/models/video.model.js
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    // we will get the duration from the cloudinary
+    duration: {
+      type: Number,
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    // is video is public or not
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
