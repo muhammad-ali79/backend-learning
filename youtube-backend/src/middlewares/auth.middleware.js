3e031c8560db65bf7bf583b0f70fe60ef4d64618
muhammad-ali79 <alihelium369@gmail.com>
2024-02-12 22:38:38 +0500

    auth middleware updated

---
+++ youtube-backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import { User } from "../models/user.model";
+import ApiError from "../utils/apiError";
+import asyncHandler from "../utils/asyncHandler";
+import Jwt from "jsonwebtoken";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    // getting the accessToken from the cookies or from header if the req come from a mobile app
+    const token =
+      req.cookies.accessToken ||
+      // accessing the authorization field and just selecting the token from the value
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) throw new ApiError(401, "Unauthorized request");
+
+    const decodedToken = Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = User.findById(decodedToken?._id).select(
+      "-password",
+      "-refreshToken"
+    );
+    if (!user) throw new ApiError(401, "Invalid Access Token");
+
+    // adding the new property on the request
+    req.user = user;
+
+    // go to next middleware or other process
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
+
+export default verifyJWT;
--- youtube-backend/src/routes/user.routes.js
+++ youtube-backend/src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -10,7 +15,12 @@ router.route("/register").post(
     { name: "avatar", maxCount: 1 },
     { name: "coverImage", maxCount: 1 },
   ]),
+
+  // .post will automitacclay passed the req,res,next
   registerUser
 );
+
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
